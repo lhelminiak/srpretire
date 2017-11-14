b6421e200ce42a52286c4dc2d8841cc3102b749f
karolgrycuk <kgrycu2@uic.edu>
2017-11-13 22:52:53 -0600

    reorganization and some changes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
      * @Route("/", name="home")
      */
     public function homepageAction(Request $request){
-        return $this->render('home.html.twig');
+        return $this->render('home_page/home_base.html.twig');
     }
 
 
@@ -25,21 +25,21 @@ class DefaultController extends Controller
      * @Route("/who-we-are", name="who-we-are")
      */
     public function whoWeArePageAction(Request $request){
-        return $this->render('who-we-are.html.twig', array("title" => "SRP-Who We Are"));
+        return $this->render('who_we_are_page/who-we-are.html.twig', array("title" => "SRP-Who We Are"));
     }
 
     /**
      * @Route("/what-we-do", name="what-we-do")
      */
     public function whatWeDoPageAction(Request $request){
-        return $this->render('what-we-do.html.twig', array("title" => "SRP-What We Do"));
+        return $this->render('what_we_do_page/what-we-do.html.twig', array("title" => "SRP-What We Do"));
     }
 
     /**
      * @Route("/how-we-do-it", name="how-we-do-it")
      */
     public function howWeDoItPageAction(Request $request){
-        return $this->render('how-we-do-it.html.twig', array("title" => "SRP-How We Do It"));
+        return $this->render('how_we_do_it_page/how-we-do-it.html.twig', array("title" => "SRP-How We Do It"));
     }
 
     /*
@@ -55,36 +55,36 @@ class DefaultController extends Controller
      * @Route("/retirement-plan-consulting", name="retirement-plan-consulting")
      */
     public function retirementPlanConsultingPageAction(Request $request){
-        return $this->render('what-we-do-processes/retirement-plan-consulting.html.twig', array("title" => "SRP-Retirement Plan Consulting"));
+        return $this->render('pages/retirement_plan_consulting_page/retirement-plan-consulting.html.twig', array("title" => "SRP-Retirement Plan Consulting"));
     }
 
     /**
      * @Route("/executive-benefits", name="executive-benefits")
      */
     public function executiveBenefitsPageAction(Request $request){
-        return $this->render('what-we-do-processes/executive-benefits.html.twig', array("title" => "SRP-Executive Benefits"));
+        return $this->render('pages/executive_benefits_page/executive-benefits.html.twig', array("title" => "SRP-Executive Benefits"));
     }
 
-    /**
-     * @Route("/endowments-foundations", name="endowments-foundations")
-     */
-    public function endowmentsFoundationsPageAction(Request $request){
-        return $this->render('what-we-do-processes/endowments-foundations.html.twig', array("title" => "SRP-Endowments and Foundations"));
-    }
+//    /**
+//     * @Route("/endowments-foundations", name="endowments-foundations")
+//     */
+//    public function endowmentsFoundationsPageAction(Request $request){
+//        return $this->render('what-we-do-processes/endowments-foundations.html.twig', array("title" => "SRP-Endowments and Foundations"));
+//    }
 
-    /**
-     * @Route("/HSA", name="HSA")
-     */
-    public function HSAPageAction(Request $request){
-        return $this->render('what-we-do-processes/executive-benefits.html.twig', array("title" => "SRP-HSA"));
-    }
-
-    /**
-     * @Route("/endowments-foundations", name="endowments-foundations")
-     */
-    public function wealthManagementPageAction(Request $request){
-        return $this->render('what-we-do-processes/executive-benefits.html.twig', array("title" => "SRP-Wealth Management"));
-    }
+//    /**
+//     * @Route("/HSA", name="HSA")
+//     */
+//    public function HSAPageAction(Request $request){
+//        return $this->render('what-we-do-processes/executive-benefits.html.twig', array("title" => "SRP-HSA"));
+//    }
+//
+//    /**
+//     * @Route("/endowments-foundations", name="endowments-foundations")
+//     */
+//    public function wealthManagementPageAction(Request $request){
+//        return $this->render('what-we-do-processes/executive-benefits.html.twig', array("title" => "SRP-Wealth Management"));
+//    }
 
     /*
      *  END What We Do processes sub sections
@@ -104,14 +104,14 @@ class DefaultController extends Controller
      * @Route("/lets-connect", name="lets-connect")
      */
     public function letsConnectPageAction(Request $request){
-        return $this->render('connect.html.twig', array("title" => "SRP-Let's Connect"));
+        return $this->render('pages/lets_connect_page/lets_connect.html.twig', array("title" => "SRP-Let's Connect"));
     }
 
     /**
      * @Route("/submit-rfp", name="submit-rfp")
      */
     public function submitRfpPageAction(Request $request){
-        return $this->render('submit_RFP.html.twig', array("title" => "SRP-Submit RFP"));
+        return $this->render('pages/submit_rfp_page/submit-RFP.html.twig', array("title" => "SRP-Submit RFP"));
     }
 
     /**
